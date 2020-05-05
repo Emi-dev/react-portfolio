32da5ff10e56b0b82d4f581f8ac7e7a03b82fe0f
Emi Clar <kotaki@gmail.com>
2020-05-04 20:06:45 -0400

    Updates

---
+++ precache-manifest.b147419d27c97e7f07724136c944d69f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf8cdc1292a959efd641022a790b0270",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "3325eba75a895a2072cb",
+    "url": "/react-portfolio/static/css/main.f8436835.chunk.css"
+  },
+  {
+    "revision": "9be472a11c8d01ef0d7d",
+    "url": "/react-portfolio/static/js/2.eee94441.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.eee94441.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3325eba75a895a2072cb",
+    "url": "/react-portfolio/static/js/main.9e05532f.chunk.js"
+  },
+  {
+    "revision": "37db3f26b7c0ea36aed0",
+    "url": "/react-portfolio/static/js/runtime-main.d08fb9a1.js"
+  }
+]);
